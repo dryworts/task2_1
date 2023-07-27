e398c4109b428cac995e11e4970488ef7e8ada02
dryworts <omzg@mail.ru>
2023-07-27 13:01:06 +0600

    work on mistakes 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    int cost = 13678;
-    int bonusMiles = cost/20;
+        int cost = 13678;
+        int bonusMiles = cost / 20;
 
-    System.out.println("Вам начислено " + bonusMiles + " бонусов.");
+        System.out.println("Вам начислено " + bonusMiles + " бонусов.");
     }
 }
